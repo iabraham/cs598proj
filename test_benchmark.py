1f37acc1c056d3a5e1a76959580cf2d851c61f69
iabraham <itabrah2@illinois.edu>
2018-11-02 17:41:32 -0500

    Modded test_benchmark.py for our use

--- test_benchmark.py
+++ test_benchmark.py
@@ -38,43 +38,69 @@ out_path = 'benchmark_results/SRF_' + str(UPSCALE_FACTOR) + '/'
 if not os.path.exists(out_path):
     os.makedirs(out_path)
 
-for image_name, lr_image, hr_restore_img, hr_image in test_bar:
+#for image_name, lr_image, hr_restore_img, hr_image in test_bar:
+#    image_name = image_name[0]
+#    lr_image = Variable(lr_image, volatile=True)
+#    hr_image = Variable(hr_image, volatile=True)
+#    if torch.cuda.is_available():
+#        lr_image = lr_image.cuda()
+#        hr_image = hr_image.cuda()
+
+#    sr_image = model(lr_image)
+#    mse = ((hr_image - sr_image) ** 2).data.mean()
+#    psnr = 10 * log10(1 / mse)
+#    ssim = pytorch_ssim.ssim(sr_image, hr_image).data[0]
+#
+#    test_images = torch.stack(
+#        [display_transform()(hr_restore_img.squeeze(0)), display_transform()(hr_image.data.cpu().squeeze(0)),
+#         display_transform()(sr_image.data.cpu().squeeze(0))])
+#    image = utils.make_grid(test_images, nrow=3, padding=5)
+#    utils.save_image(image, out_path + image_name.split('.')[0] + '_psnr_%.4f_ssim_%.4f.' % (psnr, ssim) +
+#                     image_name.split('.')[-1], padding=5)
+
+for image_name, lr_image, hr_restore_img in test_bar:
     image_name = image_name[0]
     lr_image = Variable(lr_image, volatile=True)
-    hr_image = Variable(hr_image, volatile=True)
+#    hr_image = Variable(hr_image, volatile=True)
     if torch.cuda.is_available():
         lr_image = lr_image.cuda()
-        hr_image = hr_image.cuda()
+#       hr_image = hr_image.cuda()
 
     sr_image = model(lr_image)
-    mse = ((hr_image - sr_image) ** 2).data.mean()
-    psnr = 10 * log10(1 / mse)
-    ssim = pytorch_ssim.ssim(sr_image, hr_image).data[0]
+#    mse = ((hr_image - sr_image) ** 2).data.mean()
+#    psnr = 10 * log10(1 / mse)
+#    ssim = pytorch_ssim.ssim(sr_image, hr_image).data[0]
 
-    test_images = torch.stack(
-        [display_transform()(hr_restore_img.squeeze(0)), display_transform()(hr_image.data.cpu().squeeze(0)),
-         display_transform()(sr_image.data.cpu().squeeze(0))])
-    image = utils.make_grid(test_images, nrow=3, padding=5)
-    utils.save_image(image, out_path + image_name.split('.')[0] + '_psnr_%.4f_ssim_%.4f.' % (psnr, ssim) +
-                     image_name.split('.')[-1], padding=5)
+#    test_images = torch.stack(
+#        [display_transform()(hr_restore_img.squeeze(0)), display_transform()(hr_image.data.cpu().squeeze(0)),
+#         display_transform()(sr_image.data.cpu().squeeze(0))])
+#    image = utils.make_grid(test_images, nrow=3, padding=5)
+    print(image_name.split('.')[0])
+    print(image_name.split('.')[-1])
+    utils.save_image(sr_image, out_path + image_name.split('.')[0] + '.' + image_name.split('.')[-1], padding=5)
 
     # save psnr\ssim
     results[image_name.split('_')[0]]['psnr'].append(psnr)
     results[image_name.split('_')[0]]['ssim'].append(ssim)
 
-out_path = 'statistics/'
-saved_results = {'psnr': [], 'ssim': []}
-for item in results.values():
-    psnr = np.array(item['psnr'])
-    ssim = np.array(item['ssim'])
-    if (len(psnr) == 0) or (len(ssim) == 0):
-        psnr = 'No data'
-        ssim = 'No data'
-    else:
-        psnr = psnr.mean()
-        ssim = ssim.mean()
-    saved_results['psnr'].append(psnr)
-    saved_results['ssim'].append(ssim)
 
-data_frame = pd.DataFrame(saved_results, results.keys())
-data_frame.to_csv(out_path + 'srf_' + str(UPSCALE_FACTOR) + '_test_results.csv', index_label='DataSet')
+    # save psnr\ssim
+    results[image_name.split('_')[0]]['psnr'].append(psnr)
+    results[image_name.split('_')[0]]['ssim'].append(ssim)
+
+# out_path = 'statistics/'
+# saved_results = {'psnr': [], 'ssim': []}
+# for item in results.values():
+#     psnr = np.array(item['psnr'])
+#     ssim = np.array(item['ssim'])
+#     if (len(psnr) == 0) or (len(ssim) == 0):
+#         psnr = 'No data'
+#         ssim = 'No data'
+#     else:
+#         psnr = psnr.mean()
+#         ssim = ssim.mean()
+#     saved_results['psnr'].append(psnr)
+#     saved_results['ssim'].append(ssim)
+#
+# data_frame = pd.DataFrame(saved_results, results.keys())
+# data_frame.to_csv(out_path + 'srf_' + str(UPSCALE_FACTOR) + '_test_results.csv', index_label='DataSet')
