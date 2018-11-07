e606b028728320e0550f8458b63e12c3a87c3acd
wyattsmcall1 <wyattsmcall1@me.com>
2018-11-07 15:55:32 -0600

    scripts

--- scripts/obj_parse.py
+++ scripts/obj_parse.py
@@ -1,7 +1,10 @@
 file_object  = open("000annotations.txt", "r")
+NUM_IMAGES = 450
+
 #print(file_object.read())
 files = list()
 objects = list()
+NUM_OBJECTS = 0
 for line in file_object:
     #print(line)
     words = line.split()
@@ -16,11 +19,12 @@ for line in file_object:
         for word in words:
             if word != 'Objects' and word != 'Detected:':
                 objects_line.append(word)
+                NUM_OBJECTS += 1
         objects.append(objects_line)
 #print(files)
 #print(objects)
 
-file_object  = open("000annotations2.txt", "r")
+file_object  = open("object_out_all_blurs.txt", "r")
 files_SRGAN_downsampled = list()
 objects_SRGAN_downsampled = list()
 for line in file_object:
@@ -41,15 +45,45 @@ for line in file_object:
 #print(files)
 #print(objects)
 
-err = 0
+sinc_acc = 0.0
+nearest_acc = 0.0
+cubic_acc = 0.0
+area_acc = 0.0
 for i, file in enumerate(files):
     for j, file_downsampled in enumerate(files_SRGAN_downsampled):
-        if file_downsampled == file:
+        if file_downsampled == 'sinc' + file:
             for k, classes_real in enumerate(objects[i]):
                 for h, classes_found in enumerate(objects_SRGAN_downsampled[j]):
-                    if classes_real != classes_found and k == h:
-                        err += 1
-print(err)
+                    if classes_found == classes_real:
+                        sinc_acc += 1.0
+                        objects_SRGAN_downsampled[j][h] = []
+        elif file_downsampled == 'nearest' + file:
+            for k, classes_real in enumerate(objects[i]):
+                for h, classes_found in enumerate(objects_SRGAN_downsampled[j]):
+                    if classes_found == classes_real:
+                        nearest_acc += 1.0
+                        objects_SRGAN_downsampled[j][h] = []
+        elif file_downsampled == 'cubic' + file:
+            for k, classes_real in enumerate(objects[i]):
+                for h, classes_found in enumerate(objects_SRGAN_downsampled[j]):
+                    if classes_found == classes_real:
+                        cubic_acc += 1.0
+                        objects_SRGAN_downsampled[j][h] = []
+        elif file_downsampled == 'area' + file:
+            for k, classes_real in enumerate(objects[i]):
+                for h, classes_found in enumerate(objects_SRGAN_downsampled[j]):
+                    if classes_found == classes_real:
+                        area_acc += 1.0
+                        objects_SRGAN_downsampled[j][h] = []
+sinc_acc = sinc_acc / NUM_OBJECTS
+nearest_acc = nearest_acc  / NUM_OBJECTS
+cubic_acc = cubic_acc / NUM_OBJECTS
+area_acc = area_acc / NUM_OBJECTS
+
+print(sinc_acc)
+print(nearest_acc)
+print(cubic_acc)
+print(area_acc)
 
 
 
