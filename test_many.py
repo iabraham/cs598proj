6b7b84b33ccec4f803c6a2a0a454428f2247f197
iabraham <itabrah2@illinois.edu>
2018-11-22 09:18:47 -0600

    Need to update netG/netD .pth files

--- test_many.py
+++ test_many.py
@@ -3,9 +3,7 @@ import torchvision.utils as utils
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-
-import pytorch_ssim
-from data_utils import TestFromFolder, display
+from data_utils import TestFromFolder
 from model import Generator
 
 parser = argparse.ArgumentParser(description='Test Benchmark Datasets')
@@ -16,85 +14,34 @@ opt = parser.parse_args()
 U_FACTOR = opt.upscale_factor
 MODEL_NAME = opt.model_name
 
-results = {'Set5': {'psnr': [], 'ssim': []}, 'Set14': {'psnr': [], 'ssim': []}, 'BSD100': {'psnr': [], 'ssim': []},
-           'Urban100': {'psnr': [], 'ssim': []}, 'SunHays80': {'psnr': [], 'ssim': []}}
-
 model = Generator(U_FACTOR).eval()
+
 if torch.cuda.is_available():
     model = model.cuda()
-model.load_state_dict(torch.load('epochs/' + MODEL_NAME))
+    model.load_state_dict(torch.load('epochs/' + MODEL_NAME))
+else:
+    model.load_state_dict(torch.load('epochs/' + MODEL_NAME, map_location=lambda storage, loc: storage))
 
 test_set = TestFromFolder('data/test', upscale_factor=U_FACTOR)
 test_loader = DataLoader(dataset=test_set, num_workers=4, batch_size=1, shuffle=False)
-test_bar = tqdm(test_loader, desc='[testing benchmark datasets]')
+test_bar = tqdm(test_loader, desc='[testing from folder]')
 
-out_path = 'benchmark_results/SRF_' + str(U_FACTOR) + '/'
+out_path = 'from_folder_results/SRF_' + str(U_FACTOR) + '/'
 if not os.path.exists(out_path):
     os.makedirs(out_path)
 
-#for image_name, lr_image, hr_restore_img, hr_image in test_bar:
-#    image_name = image_name[0]
-#    lr_image = Variable(lr_image, volatile=True)
-#    hr_image = Variable(hr_image, volatile=True)
-#    if torch.cuda.is_available():
-#        lr_image = lr_image.cuda()
-#        hr_image = hr_image.cuda()
-
-#    sr_image = model(lr_image)
-#    mse = ((hr_image - sr_image) ** 2).data.mean()
-#    psnr = 10 * log10(1 / mse)
-#    ssim = pytorch_ssim.ssim(sr_image, hr_image).data[0]
-#
-#    test_images = torch.stack(
-#        [display_transform()(hr_restore_img.squeeze(0)), display_transform()(hr_image.data.cpu().squeeze(0)),
-#         display_transform()(sr_image.data.cpu().squeeze(0))])
-#    image = utils.make_grid(test_images, nrow=3, padding=5)
-#    utils.save_image(image, out_path + image_name.split('.')[0] + '_psnr_%.4f_ssim_%.4f.' % (psnr, ssim) +
-#                     image_name.split('.')[-1], padding=5)
 
 for image_name, lr_image, hr_restore_img in test_bar:
     image_name = image_name[0]
-    lr_image = Variable(lr_image, volatile=True)
-#    hr_image = Variable(hr_image, volatile=True)
+
+    with torch.no_grad():
+        lr_image = Variable(lr_image)
+
     if torch.cuda.is_available():
         lr_image = lr_image.cuda()
-#       hr_image = hr_image.cuda()
 
     sr_image = model(lr_image)
-#    mse = ((hr_image - sr_image) ** 2).data.mean()
-#    psnr = 10 * log10(1 / mse)
-#    ssim = pytorch_ssim.ssim(sr_image, hr_image).data[0]
 
-#    test_images = torch.stack(
-#        [display_transform()(hr_restore_img.squeeze(0)), display_transform()(hr_image.data.cpu().squeeze(0)),
-#         display_transform()(sr_image.data.cpu().squeeze(0))])
-#    image = utils.make_grid(test_images, nrow=3, padding=5)
     print(image_name.split('.')[0])
     print(image_name.split('.')[-1])
     utils.save_image(sr_image, out_path + image_name.split('.')[0] + '.' + image_name.split('.')[-1], padding=5)
-
-    # save psnr\ssim
-    results[image_name.split('_')[0]]['psnr'].append(psnr)
-    results[image_name.split('_')[0]]['ssim'].append(ssim)
-
-
-    # save psnr\ssim
-    results[image_name.split('_')[0]]['psnr'].append(psnr)
-    results[image_name.split('_')[0]]['ssim'].append(ssim)
-
-# out_path = 'statistics/'
-# saved_results = {'psnr': [], 'ssim': []}
-# for item in results.values():
-#     psnr = np.array(item['psnr'])
-#     ssim = np.array(item['ssim'])
-#     if (len(psnr) == 0) or (len(ssim) == 0):
-#         psnr = 'No data'
-#         ssim = 'No data'
-#     else:
-#         psnr = psnr.mean()
-#         ssim = ssim.mean()
-#     saved_results['psnr'].append(psnr)
-#     saved_results['ssim'].append(ssim)
-#
-# data_frame = pd.DataFrame(saved_results, results.keys())
-# data_frame.to_csv(out_path + 'srf_' + str(UPSCALE_FACTOR) + '_test_results.csv', index_label='DataSet')
