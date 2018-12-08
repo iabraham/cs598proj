2c6063568a8f0b3921f56c7624869835efdd6492
wyattsmcall1 <wyattsmcall1@me.com>
2018-12-08 09:46:24 -0600

    update

--- test_image.py
+++ test_image.py
@@ -9,7 +9,8 @@ parser = argparse.ArgumentParser(description='Test Single Image')
 parser.add_argument('--upscale_factor', default=4, type=int, help='super resolution upscale factor')
 parser.add_argument('--test_mode', default='CPU', type=str, choices=['GPU', 'CPU'], help='using GPU or CPU')
 parser.add_argument('--image_name', type=str, help='test low resolution image name')
-parser.add_argument('--model_name', default='netG_epoch_4_100.pth', type=str, help='generator model epoch name')
+# parser.add_argument('--model_name', default='netG_epoch_4_100.pth', type=str, help='generator model epoch name')
+parser.add_argument('--model_name', default='netG_epoch_4_100_tom.pth', type=str, help='generator model epoch name')
 opt = parser.parse_args()
 
 U_FACTOR = opt.upscale_factor
@@ -20,9 +21,11 @@ model = Generator(U_FACTOR).eval()
 
 if MODE:
     model.cuda()
-    model.load_state_dict(torch.load('epochs/' + MODEL_NAME))
+    # model.load_state_dict(torch.load('epochs/' + MODEL_NAME))
+    model.load_state_dict(torch.load('../pth/' + MODEL_NAME))
 else:
-    model.load_state_dict(torch.load('epochs/' + MODEL_NAME, map_location=lambda storage, loc: storage))
+    # model.load_state_dict(torch.load('epochs/' + MODEL_NAME, map_location=lambda storage, loc: storage))
+    model.load_state_dict(torch.load('../pth/' + MODEL_NAME, map_location=lambda storage, loc: storage))
 
 image = Image.open(IMAGE_NAME)
 
@@ -37,4 +40,5 @@ out = model(image)
 elapsed = (time.process_time() - start)
 print('cost' + str(elapsed) + 's')
 out_img = ToPILImage()(out[0].data.cpu())
-out_img.save('out_srf_' + str(U_FACTOR) + '_' + IMAGE_NAME)
+# out_img.save('out_srf_' + str(U_FACTOR) + '_' + IMAGE_NAME)
+out_img.save('folder_results/' + IMAGE_NAME)
